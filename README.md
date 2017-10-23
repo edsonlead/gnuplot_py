68e36313cb077e06d7ace4eddc8cd81a8907f685
Edson Araújo <edsonlead@gmail.com>
2017-10-23 13:05:04 -0300

    Atualização de texto

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-### Apenas um teste :)
+## Gplotpy
+
+**Em Desenvolvimento**
+
+* Objetivo: executar o Gnuplot dentro de um arquivo .py
+* Requisito: Gnuplot instalado
+
+### Funcionalidades
+
+No momento, suporta as seguintes funcionalidades do Gnuplot:
+
+* Título do gráfico
+* Labels dos eixos
+* Salvar figura em .png
+* Plotar o gráfico
+
+Futuras funcionalidades:
+
+* Replot de gráfico
+* Título de linhas
+* Estilo de linhas
+* Legenda
+* outras...
+
+### Como usar
+
+    from gplotpy import Gplotpy as gp
+
+    title = gp.title("Título do Gráfico", "20")
+    label = gp.label("Label do Eixo X", "Label do Eixo Y", "14")
+    figure = gp.figure("graphlog.txt", "300", "280")
+    plot = gp.plot("graphlog.txt")
+    tpl = "new_teste.tpl"
+
+    gp.template(tpl, title, label, figure, plot)
+    gp.execute(tpl)
+
+
---
+++ gplotpy.py
@@ -0,0 +1,34 @@
+from subprocess import run
+
+class Gplotpy(): 
+
+    def title(title, font_size):
+        st = ("\nset title '{}' font '{}'".format(title, font_size))
+        return st
+
+    def label(x_label, y_label, font_size):
+        sl = ("\nset xlabel '{}'".format(x_label) +
+                "\nset ylabel '{}' font '{}'".format(y_label, font_size))
+        return sl
+
+    def figure(filename, w_size, h_size):
+        sf = ("\nset terminal pngcairo  size '{}', '{}'".format(w_size, h_size) +
+                "\nset output '{}png'".format(filename[0:-3]))
+        return sf
+
+    def plot(filename):
+        plt = ("\nplot '{}'".format(filename))
+        return plt
+
+    def template(tpl, title, label, figure, plot):
+        ctpl = open(tpl, 'w')
+        ctpl.write(title)
+        ctpl.write(label)
+        ctpl.write(figure)
+        ctpl.write(plot)
+        ctpl.close()
+
+    def execute(ctpl,command="gnuplot"):
+        etpl = run([command, ctpl])
+        return etpl
+
