bf0d66c09133497ad1b81ccb3813e75d39148e4d
GabsGear <gabriel_ghellere@hotmail.com>
2017-10-24 09:49:48 -0200

    cor e tipo de linha

--- chama_gpy.py
+++ chama_gpy.py
@@ -3,8 +3,9 @@ from gplotpy import Gplotpy as gp
 title = gp.title("Título do Gráfico", "20")
 label = gp.label("Label do Eixo X", "Label do Eixo Y", "14")
 figure = gp.figure("graphlog.txt", "300", "280")
+line = gp.line("1", "cyan")
 plot = gp.plot("graphlog.txt")
 tpl = "new_teste.tpl"
 
-gp.template(tpl, title, label, figure, plot)
+gp.template(tpl, title, label, figure, line, plot)
 gp.execute(tpl)
